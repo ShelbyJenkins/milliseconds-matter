3396f97bf561672fb000ead578b3f2e9644771c1
ShelbyJenkins <shelbyjenkins@gmail.com>
2022-08-27 23:35:40 -0500

    Adds UI for first working test.

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ window.addEventListener('load', function () {
 
 // Types text into terminal upon load.
 function introText() {
-    fetch('/terminalText.txt')
+    fetch('terminalText.txt')
     .then(response => response.text())
     .then((text) => {
       for(i = 0; i < text.length; i++) {
@@ -74,6 +74,9 @@ function runCommand(cmd) {
         case 'waf-test':
             window.location.href = '/waf/waf.html';
             break;
+            case 'waf-test-start':
+                fetchTest();
+                return;    
         case 'rpc-test':
             terminal.write('   Command not implemented.');
             terminal.write('\r\n');

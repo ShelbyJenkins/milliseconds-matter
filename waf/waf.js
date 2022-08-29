2bd8e9efc1e338ba0cedd89f6543d090b60a23f4
ShelbyJenkins <shelbyjenkins@gmail.com>
2022-08-29 15:07:30 -0500

    Adds rank sort to table.

--- waf/waf.js
+++ waf/waf.js
@@ -6,7 +6,7 @@ async function fetchTest() {
       org     : 'control',  
       url     : 'https://google.com/',
       rank    : 0,
-      percent : 0,
+      percent : 100,
       ttfb    : 0,
       actual  : 0
     },
@@ -19,8 +19,8 @@ async function fetchTest() {
       actual  : 0
     },
     { 
-      org     : 'stackpath1', 
-      url     : 'https://p4p2r9v3.stackpathcdn.com/',
+      org     : 'error.com', 
+      url     : 'https://error.com',
       rank    : 0,
       percent : 0,
       ttfb    : 0,
@@ -29,6 +29,7 @@ async function fetchTest() {
   ];
   // Defines control average within main function scope.
   let controlAverage;
+  let slowestActual = 0;
 
   // Iterates through each org and waits for completion before each.
   await orgs.reduce(async (memo, o) => {
@@ -58,15 +59,19 @@ async function fetchTest() {
     if (o.org === 'control') {
       controlAverage = a;
     }
+    // Sets slowest actual.
+    if (a > slowestActual) {
+      slowestActual = a;
+    }
     // Sets results into orgs object.
     o.ttfb = a;
-    // Skips control.
+    // Sets average results and graph in orgs object. Skips control.
     if ((a - controlAverage) > 0 ) {
       o.actual = (a - controlAverage);
-    } 
+    }
     return 1;
   }
-  console.log('unsorted: ', orgs);
+
   postTest(orgs);
 }
 
@@ -77,16 +82,13 @@ function postTest(orgs) {
   orgs.forEach((o, idx) => {
     o.rank = idx;
   });
-  console.log('sorted and ranked: ', orgs);
-  // Updates table with rank, ttfb, and actual.
+  // Populates table with rank, ttfb, and actual.
   orgs.forEach((o) => {
     updateTableFields(o);
   })
-
-  
-  // Sorts tables.
-
   // Populates graph.
+  // (old-new)/new x 100% = %
+  
 
   terminal.write('\r\n');
   terminal.write('\r\n');
@@ -94,51 +96,45 @@ function postTest(orgs) {
 }
 
 function updateTableFields(o) {
-  // Sets col variable based on org.
-  switch (o.org) {
-    case 'control':
-      var row = 0;
-      break;
-    case 'stackpath':
-      var row = 1;
-      break;
-    case 'stackpath1':
-      var row = 2;
-      break;
-    case 'imperva':
-      var row = 2;
-    case 'aws':
-      var row = 3;
-    case 'cloudflare':
-      var row = 4;
-    case 'fastly':
-      var row = 5;
-  }
+  // Sets col variable based on rank.
+  var row = o.rank;
   // Iterate through each field to be updated.
-  const fields = ['rank', 'ttfb', 'actual'];
+  const fields = ['org', 'percent', 'rank', 'ttfb', 'actual'];
   fields.forEach((f) => {
-    console.log(f)
     switch (f) {
-      case 'rank':
-        var col = 1;
-        var update = o.rank;
+      case 'org':
+        var update = o.org;
+        fields(0);
         break;
+      case 'rank':
+        var update = o.rank;
+        fields(1);
+        break;
+      // case 'percent':
+      //   var update = o.percent;
+      //   fields(2);
+      //   break;  
       case 'ttfb':
-        var col = 3;
         var update = Math.round(o.ttfb * 10) / 10 + 'ms';
+        fields(3);
         break;
       case 'actual':
-        var col = 4;
         var update = Math.round(o.actual * 10) / 10 + 'ms';
+        fields(4);
         break;
     }
-    // Updates table!
-    myBody = document.getElementsByTagName("body")[0];
-    myTable = myBody.getElementsByTagName("table")[0];
-    myTableBody = myTable.getElementsByTagName("tbody")[0];
-    myRow = myTableBody.getElementsByTagName("tr")[row];
-    myCell = myRow.getElementsByTagName("td")[col];
-    myCell.innerText = update;
+    function fields(col) {
+      myBody = document.getElementsByTagName("body")[0];
+      myTable = myBody.getElementsByTagName("table")[0];
+      myTableBody = myTable.getElementsByTagName("tbody")[0];
+      myRow = myTableBody.getElementsByTagName("tr")[row];
+      myCell = myRow.getElementsByTagName("td")[col];
+      myCell.innerText = update;
+    }
+    function graph(percent) {
+
+    }
+
   });
   
 }
